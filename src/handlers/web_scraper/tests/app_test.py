d4fed68e0a68834cef0bdf92068f6abc937b8bdc
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2021-01-09 13:08:30 -0600

    fix: Fix the linter issues.

--- src/handlers/web_scraper/tests/app_test.py
+++ src/handlers/web_scraper/tests/app_test.py
@@ -1,17 +1,17 @@
-from mamba import context, describe, it
 from expects import equal, expect
+from mamba import context, describe, it
 
 from src.handlers.web_scraper.app import add
 
 with describe(add):
     with context('when receives 1 + 1'):
-        with it('should return 2'):        
+        with it('should return 2'):
             actual_result = add(1, 1)
 
             expect(actual_result).to(equal(2))
 
     with context('when receives 4 + 1'):
-        with it('should return 5'):        
+        with it('should return 5'):
             actual_result = add(4, 1)
 
             expect(actual_result).to(equal(5))
