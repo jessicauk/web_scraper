9e37563f7a36917578705d2f935a5be2adf07f13
Jessica Apolinar <jessica.uk220691@gmail.com>
2021-01-16 14:05:37 -0600

    feat: Add spider.

--- src/handlers/web_scraper/tests/app_test.py
+++ src/handlers/web_scraper/tests/app_test.py
@@ -1,17 +1,11 @@
 from expects import equal, expect
 from mamba import context, describe, it
 
-from src.handlers.web_scraper.app import add
+from src.handlers.web_scraper.app import Spider
 
-with describe(add):
-    with context('when receives 1 + 1'):
-        with it('should return 2'):
-            actual_result = add(1, 1)
+with describe(Spider):
+    with describe('parse'):
+        with context('when the response is valid'):
+            with it('should return parsed response'):
 
-            expect(actual_result).to(equal(2))
 
-    with context('when receives 4 + 1'):
-        with it('should return 5'):
-            actual_result = add(4, 1)
-
-            expect(actual_result).to(equal(5))
